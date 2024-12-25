2e8064636b0b62065aadefab8bd01d1199a2baf3
ProudGuy69 <idioticbatteringram@gmail.com>
2024-12-24 20:32:52 -0500

    new profile functions

--- database/matchingdb.py
+++ database/matchingdb.py
@@ -3,7 +3,7 @@ from discord.ext.commands import Bot
 from database.databasev2 import MATCHING, NoProfileException
 from time import time
 
-from typing import List
+import random
 
 # puts the profile in a queue to be verifed
 
@@ -33,35 +33,78 @@ class Profile():
         self.paired_with_us = data.get('paired_with_us', [])
         self.tos = data.get('tos_agreed')
         self.data:dict = data
+
     
-    def __call__(self):
-        profiles = []
+    def compare(self, other:"Profile") -> bool:
+        """compares to profiles to see if they are compatible
 
-        for profile in get_compatible(self.user):
+        Args:
+            other (Profile): the other profile to compare against ours
+
+        Returns:
+            bool: True if compatible, false if not
+        """
+        if self == other: return False
+        if not self.approved or not other.approved: return False
+        if other.id in self.rejected_pairs: return False
+
+        our_age = self.age
+        their_age = other.age
+        age_range = our_age + 2  - their_age
+        if age_range <= 0 and age_range >=4: return False
+
+        if (
+            other.id not in self.rejected_pairs
+            and self.id not in other.selected_pairs
+            and other.id not in self.selected_pairs
+            ):
+            return True
+        else: return False
+
+    def get_compatible_profiles(self) -> list["Profile"]:
+        """Generates a list of all compatible profiles for this user
+
+        Returns:
+            list[Profile]: a list of profile objects we are compatible with
+        """
+        approved_profiles = MATCHING.find({'approved':True})
+        compatible_profiles = []
+        for profile in approved_profiles:
             try:
-                get_profile(profile.get('user_id'), self.bot)
-                profiles.append(profile)
-            except UserNotFoundException:
-                continue
-        return profiles[:10]
+                other_profile = Profile(self.bot, profile)
+            except: continue
+            if not self.compare(other_profile): continue
+            compatible_profiles.append(other_profile)
+        
+        return compatible_profiles
+    
 
-    def fetch_data(self):
-        return MATCHING.find_one({'_id':self._id})
+    def get_random_profile(self) -> "Profile":
+        """randomly grabs a compatible profile
+
+        Returns:
+            Profile: a random profile
+        """
+        compatible_profiles = self.get_compatible_profiles()
+        return compatible_profiles[0, len(compatible_profiles)-1]
+
+
+        
+
+        
 
     def edit(self, data, upsert=False):
         result = MATCHING.update_one({'_id':self._id}, data, upsert=upsert)
         return result
     
     def generate_embed(self, color=0xffa1dc):
-        description = f"""
-        ❥﹒User: {self.user.mention} | `{self.user.global_name}`
-        ❥﹒Name: `{self.name}`
-        ❥﹒Pronouns: `{self.pronouns}`
-        ❥﹒Gender: `{self.gender}`
-        ❥﹒Age: `{self.age}`
-        ❥﹒Sexuality: `{self.sexuality}`
-        ❥﹒Bio:\n```{self.bio}```
-        """
+        description = f"""❥﹒User: {self.user.mention} | `{self.user.global_name}`
+❥﹒Name: `{self.name}`
+❥﹒Pronouns: `{self.pronouns}`
+❥﹒Gender: `{self.gender}`
+❥﹒Age: `{self.age}`
+❥﹒Sexuality: `{self.sexuality}`
+❥﹒Bio:\n```{self.bio}```"""
 
         profile_embed = Embed(
         title="Profile",
@@ -78,21 +121,15 @@ class Profile():
         
 
 
-
-
 def qet_queued(message_id:int, bot) -> Profile:
-    """
-    gets the profile of a user if they are queued
+    """Grabs the queued profile with the provided message_id
 
-    Parameters
-    ----------
-    user : int
-        the message_id to get the queued data on
+    Args:
+        message_id (int): the message_id provided
+        bot (discord.Bot): discord Bot class to return a profile
 
-    Returns
-    -------
-    dict | none
-        returns a dict of the users profile
+    Returns:
+        Profile: the profile thats queued
     """
     data = MATCHING.find_one({'message_id':message_id}) or None
     return Profile(bot, data)
@@ -101,18 +138,17 @@ def qet_queued(message_id:int, bot) -> Profile:
 
 
 def get_profile(user:Member, bot:Bot) -> Profile:
-    """
-    gets a users profile
+    """Gets a profile with a provided user
 
-    Parameters
-    ----------
-    user : discord.Member
-        the user of the profile you wish to edit
-    
-    Returns
-    -------
-    dict | None
-        returns a dict of the users profile data or None
+    Args:
+        user (discord.Member): ther user's profile toget
+        bot (Bot): discord.Bot class to check if the user is in the scope of the bot
+
+    Raises:
+        UserNotFoundException: Raises if the user is not found
+
+    Returns:
+        Profile: the requests profile
     """
     if not user:
         raise UserNotFoundException()
@@ -122,90 +158,5 @@ def get_profile(user:Member, bot:Bot) -> Profile:
 
 
 
-# TODO
-### MAKE THESE PART OF PROFILE
-def compatibility_check(user_a_id:int, user_b_id:int, ignore_selected:bool=False) -> bool:
-    """
-    checks the compatibility of 2 users
-
-    compatibility params:
-    age is within 2 years of each other
-    profiles are both approved
-    profiles havent already selected each other
-
-    Parameters
-    ----------
-    user_a : int
-        the id of first user to check
-    user_b : discord.Member
-        the id of second user to check 
-    
-    Returns
-    -------
-    bool 
-        returns true if compatible, false if not
-    """
-
-    data_a:dict = MATCHING.find_one({'user_id': user_a_id})
-    data_b:dict = MATCHING.find_one({'user_id': user_b_id})
-
-    age_a = int(data_a.get('age', 0))
-    age_b = int(data_b.get('age', 100))
-
-    # if between 0-4 then we compatible
-    range = age_a + 2 - age_b
-
-    if ignore_selected:
-        if (
-            data_a != data_b
-            and data_b.get('approved') == True
-            and data_a.get('approved') == True
-            and range >= 0 and range <=4   
-            and data_b.get('user_id') not in data_a.get('rejected_pairs')   
-        ):
-            return True
-        else:
-            return False
-        
-
-    if (
-        data_a != data_b
-        and data_b.get('approved') == True
-        and data_a.get('approved') == True
-        and range >= 0 and range <=4
-        and data_a.get('user_id') not in data_b.get('selected_pairs', [])
-        and data_b.get('user_id') not in data_a.get('selected_pairs', [])
-        and data_b.get('user_id') not in data_a.get('rejected_pairs')
-    ):
-        return True
-    else:
-        return False
-
-
-
-def get_compatible(user:Member, server_only=False, ignore_selected=False) -> list[dict] | None:
-    """
-    gets all the compatible users for our user
-
-    Parameters
-    ----------
-    user : discord.Member
-        the user to generate the compatibility list
-    
-    Returns
-    -------
-    list[discord.Member] | False
-        returns a list of all discord.Members that are compatible, or false is current user isnt approved
-    """
-    user_data = get_profile(user)
-    if not user_data or user_data.get('approved') != True: return []
-
-    profiles = []
-
-    for profile in MATCHING.find({'approved': True}):
-        if not compatibility_check(user.id, profile.get('user_id'), ignore_selected=ignore_selected): continue
-        profiles.append(profile)
-    
-    return profiles
 
 
